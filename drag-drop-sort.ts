b1bc3faaf4b363921c213448340e51d3b692134d
Angular Software Engineer <thalyssonl17@gmail.com>
2022-12-10 14:26:13 -0300

    Update drag-drop-sort.ts

--- drag-drop-sort.ts
+++ drag-drop-sort.ts
@@ -1,8 +1,3 @@
-import { Injectable } from '@angular/core';
-@Injectable({
-  providedIn: 'root'
-})
-
 export class DragAndDropListLib {
   /*        __----------------------------__       */
   /*-=====_| INITIALIZING GLOBAL VARIABLES |_=====-*/
